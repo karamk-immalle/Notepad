decbe7b3993ffd6617687726ff6e8c22fe3f3459
karam.khalil <karam.khalil@B209-07>
2017-03-28 13:46:22 +0200

    Notepad update

--- WpfApplication5/MainWindow.xaml.cs
+++ WpfApplication5/MainWindow.xaml.cs
@@ -22,14 +22,21 @@ namespace WpfApplication5
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Persoon> ps = new List<Persoon>();
         private string currentFile = "";
         private string initialDir;
+        
         public MainWindow()
         {
             InitializeComponent();
             initialDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ps.Add(new Persoon() { Voornaam = "Jos", Achternaam = "Joske" });
+            datagrid.ItemsSource = ps;
+            string fileContents = textBox.Text;
         }
+       
 
+        
         private void OpenItem_Click(object sender, RoutedEventArgs e)
         {
             StreamReader inputStream;
@@ -62,12 +69,61 @@ namespace WpfApplication5
 
         private void ExitItem_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void HelpItem_Click(object sender, RoutedEventArgs e)
         {
+           
+        }
 
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Parse_Click(object sender, RoutedEventArgs e)
+        {
+            var parsedPersonen = new List<Persoon>();
+
+            string[] filedata = textBox.Text.Split('\n');
+            try
+            {
+                foreach (var row in filedata)
+                {
+                    // MessageBox.Show(String.Format("[{0}]", row.Trim()));
+
+                    // is row valid?
+                    string[] fields = row.Trim().Split(';');
+
+                    if (fields.Length != 3)
+                    {
+                        MessageBox.Show(
+                            String.Format("Couldn't parse [{0}]. Number of fields: {1}.",
+                                row.Trim(),
+                                fields.Length)
+                        );
+                    }
+                    else if (row.Trim() == "")
+                    {
+                        // ignore empty rows
+                    }
+                    else
+                    {
+                        var p = new Persoon();
+                        p.Voornaam = fields[0];
+                        p.Achternaam = fields[1];
+                        p.GeboorteDatum = DateTime.Parse(fields[2]);
+                        parsedPersonen.Add(p);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+            ps.AddRange(parsedPersonen);
+            datagrid.ItemsSource = ps;
         }
     }
 }
---
+++ WpfApplication5/Persoon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication5
+{
+    class Persoon
+    {
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public DateTime GeboorteDatum { get; set; }
+
+        public override string ToString()
+        {
+            return Voornaam + "  "
+                + Achternaam
+                + String.Format("({0})", GeboorteDatum);
+        }
+    }
+}
